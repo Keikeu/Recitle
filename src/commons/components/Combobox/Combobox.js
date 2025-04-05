c2862071f5d091f0bf51c734edbc3ee30b7ba2ec
Keikeu <karolina.m.placek@gmail.com>
2025-04-05 23:00:45 +0200

    fix: mobile popover perchance

--- src/commons/components/Combobox/Combobox.js
+++ src/commons/components/Combobox/Combobox.js
@@ -8,7 +8,7 @@ import OptionList from "../OptionList";
 
 const Box = styled.div``;
 
-function Combobox({ className, label, placeholder, value, onChange, options = [], ...rest }) {
+function Combobox({ className, label, placeholder, value, onChange, options = [], popoverPlacement, ...rest }) {
   const [searchPhrase, setSearchPhrase] = useState("");
   const selectedItemLabel = options.find(el => el.id === value)?.label || "";
 
@@ -44,9 +44,10 @@ function Combobox({ className, label, placeholder, value, onChange, options = []
           <OptionList options={filteredOptions} selectedOptionId={0} onChange={onChange} closePopover={closePopover} />
         )}
         contentStyles={{
-          maxHeight: "400px",
+          maxHeight: "40vh",
           overflow: "auto",
         }}
+        placement={popoverPlacement}
         isFullWidth
       />
     </Box>
@@ -60,6 +61,23 @@ Combobox.propTypes = {
   options: T.array,
   onChange: T.func,
   value: T.string,
+  popoverPlacement: T.oneOf([
+    "auto",
+    "auto-start",
+    "auto-end",
+    "bottom-end",
+    "bottom-start",
+    "bottom",
+    "left-end",
+    "left-start",
+    "left",
+    "right-end",
+    "right-start",
+    "right",
+    "top-end",
+    "top-start",
+    "top",
+  ]),
 };
 
 export default Combobox;
