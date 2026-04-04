73e2b30868b5ffaa3080a82556c778af2caf5477
Keikeu <karolina.m.placek@gmail.com>
2026-04-04 18:28:11 +0200

    update: reset dates

---
+++ src/data/update_dates.py
@@ -0,0 +1,17 @@
+from datetime import date, timedelta
+
+with open("songs.const.js", "r") as f:
+    content = f.read()
+
+old_start = date(2026, 4, 3)
+new_start = date(2026, 1, 1) # <- change this each time
+
+for i in range(24):
+    old = (old_start + timedelta(days=i)).isoformat()
+    new = (new_start + timedelta(days=i)).isoformat()
+    content = content.replace(f'date: "{old}"', f'date: "{new}"')
+
+with open("songs.const.js", "w") as f:
+    f.write(content)
+
+print("Done!")
